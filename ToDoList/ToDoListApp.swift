7a568f9f1cb03f4af5df7b15c97ea1e3c751d29e
雷翎 <leiling@leilingdeMacBook-Air.local>
2023-05-15 01:12:50 +0800

    First commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by 雷翎 on 2023/5/15.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
