d5d444c359d15770bada605b01f8e66202254b96
Lei <leiling@leilingdeAir.lan>
2023-05-16 16:45:00 +0800

    Reorganized files.

---
+++ ToDoList/QuoteRow.swift
@@ -0,0 +1,50 @@
+//
+//  QuoteRow.swift
+//  ToDoList
+//
+//  Created by Lei on 2023/5/16.
+//
+
+import SwiftUI
+
+struct QuoteRow: View {
+    let quote: Quote
+    
+    var body: some View {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(quote.content ?? "")
+                        .font(.title3)
+                    Spacer()
+                    HStack {
+                        Text("Added Date:")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                        Text(quote.dateAdded ?? "")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    HStack {
+                        Text("Modified Date:")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                        Text(quote.dateModified ?? "")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    HStack {
+                        Text("Author:")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                        Text(quote.author ?? "")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    
+                }
+                .fixedSize(horizontal: false, vertical: true)
+            }
+            .padding(.vertical, 12)
+        
+    }
+}
---
+++ ToDoList/ToDoListRow.swift
@@ -0,0 +1,53 @@
+//
+//  ToDoListRow.swift
+//  ToDoList
+//
+//  Created by Lei on 2023/5/16.
+//
+
+import SwiftUI
+
+struct ToDoListRow: View {
+    let toDoItem: ToDoItem
+    private let viewModel = ToDoListViewModel()
+    
+    var body: some View {
+        NavigationLink(destination: EditItemView(item: toDoItem)) {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(toDoItem.title)
+                        .font(.title2)
+                    Text(toDoItem.description)
+                        .font(.title3)
+                        .foregroundColor(.gray)
+                    Spacer()
+                    HStack {
+                        Text("created Date:")
+                            .font(.subheadline)
+                        Text(viewModel.dateToString(toDoItem.createdDate))
+                            .font(.subheadline)
+                    }
+                    HStack {
+                        Text("Due Date:")
+                            .font(.subheadline)
+                            .foregroundColor(.red)
+                        Text(viewModel.dateToString(toDoItem.dueDate))
+                            .font(.subheadline)
+                            .foregroundColor(.red)
+                    }
+                    HStack {
+                        Text("Location:")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                        Text(toDoItem.location)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    
+                }
+                .fixedSize(horizontal: false, vertical: true)
+            }
+            .padding(.vertical, 12)
+        }
+    }
+}
