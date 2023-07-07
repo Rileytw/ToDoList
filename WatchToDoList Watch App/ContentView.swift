05d110bc93b860bbbe8c1f53e5782f85851ca746
Lei <leiling@leilingdeMacBook-Air.local>
2023-07-07 22:06:14 +0800

    Add WatchOS Target.

---
+++ WatchToDoList Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WatchToDoList Watch App
+//
+//  Created by Lei on 2023/7/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
